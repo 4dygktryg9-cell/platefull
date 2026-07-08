21977cee2deef2adb317b8013867d37495ec20e9
4dygktryg9-cell <4dygktryg9@privaterelay.appleid.com>
2026-07-08 11:24:04 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "plateful-v1";
+const CACHE_NAME = "plateful-v2";
 const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
